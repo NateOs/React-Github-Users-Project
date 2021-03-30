ed6827c63b94262fd64bc1fe99510f2a1a6ecedf
nateos <sodjanathan@gmail.com>
2021-03-30 23:11:21 +0000

    Setting up context API

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+// import { GithubContext } from '../context/context';
+import {useGlobalContext} from '../context/context';
+
 const Dashboard = () => {
+  const data = useGlobalContext()
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      <p>{data}</p>
+      <Navbar></Navbar>
+      <Search />
+      <Info />
+      <User />
+      <Repos/>
     </main>
   );
 };
