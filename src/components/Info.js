6d68525aeb2ab2c5ba405f882f5c7ef627d8de77
nateos <sodjanathan@gmail.com>
2021-03-31 11:08:14 +0000

    Info component complete

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,64 @@
 import React from 'react';
-import { GithubContext } from '../context/context';
+import { useGlobalContext } from '../context/context';
 import styled from 'styled-components';
 import { GoRepo, GoGist } from 'react-icons/go';
 import { FiUsers, FiUserPlus } from 'react-icons/fi';
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
+  const { githubUser } = useGlobalContext()
+  const { public_repos, followers, following, public_gists } = githubUser
+
+  //* Destructure item values to create an iterable object
+  const items = [
+    {
+      id: 1,
+      icon: <GoRepo className='icon' />,
+      label: 'repos',
+      value: public_repos,
+      color: 'pink'
+    },
+    {
+      id: 2,
+      icon: <FiUserPlus className='icon' />,
+      label: 'followers',
+      value: followers,
+      color: 'green'
+    },
+    {
+      id: 3,
+      icon: <FiUsers className='icon' />,
+      label: 'following',
+      value: following,
+      color: 'purple'
+    },
+    {
+      id: 4,
+      icon: <GoGist className='icon' />,
+      label: 'gists',
+      value: public_gists,
+      color: 'yellow'
+    },
+  ]
+  return (
+    <Wrapper>
+      {items.map((item) => {
+        return <Item key={item.id} {...item}></Item>
+      })}
+    </Wrapper>)
 };
+
+//* Actual item component to be returned,takes item object as prop
+const Item = ({ icon, label, value, color}) => {
+  return (
+    <article className='item'>
+      <span className={color}>{icon}</span>
+      <div>
+        <h3>{value}</h3>
+        <p>{label}</p>
+      </div>
+    </article>
+  )
+}
 
 const Wrapper = styled.section`
   display: grid;
